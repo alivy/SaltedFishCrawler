2c2068db8b78fd7456f81321436ac9c65e1a2355
严浩淼 <527758393@qq.com>
2019-07-31 09:16:09 +0800

    添加返回数据对象

---
+++ Data/Model/ResMessage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection;
+using Utils.Help.CustomAttribute;
+
+namespace ViewModel
+{
+    public class ResMessage
+    {
+        /// <summary>
+        /// 状态码
+        /// </summary>
+        public int code { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string msg { get; set; }
+
+        public object data { get; set; }
+
+        public static ResMessage CreatMessage(Enum em)
+        {
+            return new ResMessage
+            {
+                code = Convert.ToInt32(em),
+                msg = em.GetRemark(),
+                data = ""
+            };
+        }
+
+
+        public static ResMessage CreatMessage(Enum em, string msg)
+        {
+            return new ResMessage
+            {
+                code = Convert.ToInt32(em),
+                msg = msg,
+                data = ""
+            };
+        }
+
+
+        public static ResMessage CreatMessage(Enum em, string msg, object data)
+        {
+            return new ResMessage
+            {
+                code = Convert.ToInt32(em),
+                msg = msg ?? em.GetRemark(),
+                data = data ?? ""
+            };
+        }
+    }
+
+
+
+}
